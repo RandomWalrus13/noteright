e30db8137eed2b4a9ad6924648c4c09cacf06c54
RandomWalrus13 <111152655+RandomWalrus13@users.noreply.github.com>
2023-02-21 14:39:15 -0600

    update

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello github world!')
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow
+)
+
+
+class NoteRightWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    app.setStyle("fusion")
+    window = NoteRightWindow()
+    window.show()
+    app.exec()
